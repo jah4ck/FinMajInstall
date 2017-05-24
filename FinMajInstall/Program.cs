56dfe5b50abd238a45a0ba304a8d1adb600cb862
jah4ck <johann.simon.web@gmail.com>
2017-05-24 17:05:10 +0200

    modif pour V2

--- FinMajInstall/Program.cs
+++ FinMajInstall/Program.cs
@@ -61,7 +61,7 @@ namespace FinMajInstall
             //remonté du log d'installation
             MyTrace.WriteLog("Début récupération log d'installation ", 2, codeappli);
             DateTime dateTraitement = DateTime.Now;
-            string pathFile = @"C:\ProgramData\CtrlPc\LOG\Install.log";
+            string pathFile = @"C:\ProgramData\CtrlPc\LOGS\Log_INSTALLPACKAGE_"+DateTime.Now.ToString("yyyyMMdd")+".log";
             if (File.Exists(pathFile))
             {
                 string[] ligne = File.ReadAllLines(pathFile, Encoding.Default);
@@ -94,7 +94,7 @@ namespace FinMajInstall
                     {
                         
                     }
-            }
+                }
                 try
                 {
                     MyTrace.WriteLog("Suppression du log d'installation ", 2, codeappli);
@@ -118,77 +118,6 @@ namespace FinMajInstall
             {
                 MyTrace.WriteLog(err.Message, 1, codeappli);
             }
-
-            //remonté du journal
-            string pathJournal = @"C:\ProgramData\CtrlPc\LOG\Journal.log";
-            string pathJournalTransfert = @"C:\ProgramData\CtrlPc\LOG\Journal_transfert.log";
-            if (File.Exists(pathJournal))
-            {
-                try
-                {
-                    File.Move(pathJournal, pathJournalTransfert);
-                    if (File.Exists(pathJournalTransfert))
-                    {
-                        string[] ligne = File.ReadAllLines(pathJournalTransfert);
-                        string colonne1 = "";
-                        string codeappli2 = "";
-                        string statut = "";
-                        string colonne4 = "";
-                        int codeerreur = 0;
-                        foreach (string line in ligne)
-                        {
-                            if (line.Length > 5)
-                            {
-                                if (line.Substring(0, 5).Contains("/"))
-                                {
-                                    colonne1 = line.Substring(0, 19);
-                                    codeappli2 = line.Substring(24, line.LastIndexOf("    ") - 24);
-                                    statut = line.Substring(line.LastIndexOf("     ") + 5, line.IndexOf(" : ", line.LastIndexOf("     ") + 5) - line.LastIndexOf("     ") - 5);
-                                    colonne4 = line.Substring(line.IndexOf(" : ", line.LastIndexOf("     ") + 5) + 3);
-                                    colonne1 = colonne1.Trim();
-                                    codeappli2 = codeappli2.Trim();
-                                    statut = statut.Trim();
-                                    if (colonne4.Contains("'"))
-                                    {
-                                        colonne4 = colonne4.Replace("'", "''");
-                                    }
-                                    
-                                    if (statut == "INFO")
-                                    {
-                                        codeerreur = 2;
-                                    }
-                                    else
-                                    {
-                                        codeerreur = 1;
-                                    }
-                                    // Console.WriteLine(colonne4);
-                                    try
-                                    {
-                                        dateTraitement = Convert.ToDateTime(colonne1);
-                                        ws.TraceLog(Guid.ToString(), dateTraitement, codeappli2, codeerreur, colonne4);
-                                        //Console.WriteLine(dateTraitement.ToString("yyyyMMddHHmmss") + ";" + colonne2);
-                                    }
-                                    catch (Exception)
-                                    {
-                                        ws.TraceLog(Guid.ToString(), dateTraitement, codeappli2, codeerreur, line);
-                                        //Console.WriteLine(dateTraitement.ToString("yyyyMMddHHmmss")+";"+line);
-                                    }
-                                }
-                                
-                            }
-                            //Console.WriteLine(colonne1);
-                        }
-                    }
-                    File.Delete(pathJournalTransfert);
-                }
-                catch (Exception err)
-                {
-                    
-                }
-            }
-            
-
-
         }
     }
 }
